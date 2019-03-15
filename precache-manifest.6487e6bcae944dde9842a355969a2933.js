0a567c8a36911b03f46b1e8ace86bdba1767b9d5
shayannasir <sshayann7@gmail.com>
2019-03-15 18:06:35 +0530

    Updates

---
+++ precache-manifest.6487e6bcae944dde9842a355969a2933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b912b765ed207c54e8",
+    "url": "/phone-directory/static/js/runtime~main.e30a5ef0.js"
+  },
+  {
+    "revision": "bc242fa7840d0ab47fa1",
+    "url": "/phone-directory/static/js/main.2a88f4cc.chunk.js"
+  },
+  {
+    "revision": "ccab78f6614968b8df0f",
+    "url": "/phone-directory/static/js/2.98e36f65.chunk.js"
+  },
+  {
+    "revision": "bc242fa7840d0ab47fa1",
+    "url": "/phone-directory/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "fc41195524c1d74a943904ba6aa60e6d",
+    "url": "/phone-directory/index.html"
+  }
+];
